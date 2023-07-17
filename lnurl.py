a21d0a07e414ffa941afa70b9644146faa9887e2
Tiago Vasconcelos <talvasconcelos@gmail.com>
2023-07-17 10:13:10 +0100

    check .onion and change scheme to http

--- lnurl.py
+++ lnurl.py
@@ -1,6 +1,7 @@
 import json
 from datetime import datetime
 from http import HTTPStatus
+from urllib.parse import urlparse
 
 import httpx
 import shortuuid
@@ -37,6 +38,12 @@ async def api_lnurl_response(request: Request, unique_hash):
             status_code=HTTPStatus.NOT_FOUND, detail="Withdraw is spent."
         )
     url = request.url_for("withdraw.api_lnurl_callback", unique_hash=link.unique_hash)
+
+    # Check if url is .onion and change to http
+    if urlparse(url).netloc.endswith(".onion"):
+        # change url string scheme to http
+        url = url.replace("https://", "http://")
+
     withdrawResponse = {
         "tag": "withdrawRequest",
         "callback": url,
@@ -189,6 +196,12 @@ async def api_lnurl_multi_response(request: Request, unique_hash, id_unique_hash
         )
 
     url = request.url_for("withdraw.api_lnurl_callback", unique_hash=link.unique_hash)
+
+    # Check if url is .onion and change to http
+    if urlparse(url).netloc.endswith(".onion"):
+        # change url string scheme to http
+        url = url.replace("https://", "http://")
+
     withdrawResponse = {
         "tag": "withdrawRequest",
         "callback": url + "?id_unique_hash=" + id_unique_hash,
